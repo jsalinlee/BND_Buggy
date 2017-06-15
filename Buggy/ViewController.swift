89571b6e750244478767ab637cef6652fc8a932a
Jonathan Lee <jsalinlee@gmail.com>
2017-06-15 15:43:39 -0700

    Initial commit. Big Nerd Ranch iOS debugging exercise. Learning how to use LLDB debugger in Xcode

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Methods: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
     }
-
 
 }
 
